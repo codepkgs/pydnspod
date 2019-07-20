989b563da46bad8f59636ca86bc47dd97065d684
zhanghe <zhanghe@maka.im>
2019-07-20 11:14:43 +0800

    modify user.py

--- dnspod/user.py
+++ dnspod/user.py
@@ -1,22 +1,100 @@
+import json
 
-from pydnspod.dnspod import
+from dnspod import Dnspod
 
 
 class User(Dnspod):
     """
     dnspod account information
     """
-    
-    def __init__(self, login_token):
-        super(User, self).__init__(login_token)
 
     def detail(self):
-        self.uri = '/User.Detail'
-        ret = self._do_request(self.uri)
-        return ret
+        """
+        get user detail information
+        :return:
+        """
+        uri = '/User.Detail'
+        ret = self._do_request(uri)
+        return ret.get('info').get('user')
 
+    def modify_userinfo(self, lastname='', title='先生', real_name='', telephone=''):
+        """
+        modify user information
+        :param lastname: your lastname, only one character
+        :param title: your title, 先生" or "女士"
+        :param real_name: your real_name
+        :param telephone: your telephone
+        :return: if modify success return True, else return False or throw Exception
+        """
+        payload = {}
 
-if __name__ == '__main__':
-    login_token = '108196,e494731ad1b1672fecb4866d8bd968eb'
-    user = User(login_token)
-    print(user.detail())
+        if lastname and len(lastname) != 1:
+            raise ValueError('lastname length must be 1 char')
+
+        if title and title not in ('先生', '女士'):
+            raise ValueError('title muse be "先生" or "女士"')
+
+        nick = '{} {}'.format(lastname, title)
+        payload['nick'] = nick
+
+        if real_name:
+            payload['real_name'] = real_name
+
+        if telephone:
+            payload['telephone'] = telephone
+
+        uri = '/User.Modify'
+        ret = self._do_request(uri, **payload)
+        if ret.get('status', {}).get('code') == 1:
+            return True
+        else:
+            return False
+
+    def modify_password(self, old_password, new_password):
+        """
+        modify user password, this function may be paused by dnspod
+        :param old_password: old password
+        :param new_password: new password
+        :return: True or False or throw Exception
+        """
+        payload = {
+            'old_password': old_password,
+            'new_password': new_password
+        }
+
+        uri = '/Userpasswd.Modify'
+        ret = self._do_request(uri, **payload)
+        if ret.get('status', {}).get('code') == 1:
+            return True
+        else:
+            return False
+
+    def modify_email(self, old_email, new_email, password):
+        """
+        modify user email, this function may be paused by dnspod
+        :param old_email: old email
+        :param new_email: new email
+        :param password: current login password
+        :return:
+        """
+        payload = {
+            'old_email': old_email,
+            'new_email': new_email,
+            'password': password
+        }
+
+        uri = '/Useremail.Modify'
+        ret = self._do_request(uri, **payload)
+        if ret.get('status', {}).get('code') == 1:
+            return True
+        else:
+            return False
+
+    def user_log(self):
+        """
+        get user operate logs
+        :return: log list
+        """
+        uri = '/User.Log'
+        ret = self._do_request(uri)
+        return ret.get('log')
